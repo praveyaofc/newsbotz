0e0746319715a0f3ffaa0f67820eada820b04894
PRAVEEN NIMSARA <151917057+praveyaofc@users.noreply.github.com>
2024-05-17 13:37:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,122 @@
-let cluster = require('cluster')
-let path = require('path')
-let fs = require('fs')
-const Readline = require('readline')
+const axios = require("axios"); 
+ const mongoose = require('mongoose'); 
+ const CryptoJS = require("crypto-js"); 
+const makeWASocket = require("@whiskeysockets/baileys").default
+const { delay ,Browsers,MessageRetryMap,fetchLatestBaileysVersion,WA_DEFAULT_EPHEMERAL,useMultiFileAuthState,makeInMemoryStore } = require("@whiskeysockets/baileys")
+    const pino = require("pino");
+const request = require('@cypress/request');
+// replace the value below with the Telegram token you receive from @BotFather
 
-var isRunning = false
-/**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
-function start(file) {
-    if (isRunning) return
-    isRunning = true
-    let args = [path.join(__dirname, file),
-        ...process.argv.slice(2)]
+ const UserSchema = new mongoose.Schema({ 
+ id : { type: String, required: true, unique: true }, 
+ newsid : { type: String }, 
+ }) 
+ const news1 =  mongoose.model("news1", UserSchema) 
+  
+  
+  
+         async function XAsena() { 
+             mongoose.connect('mongodb+srv://nipuna2007:nipuna2007@cluster0.xzonoy7.mongodb.net/?retryWrites=true&w=majority') 
+   .then(() => console.log('Connected!')); 
+  
+             const { state, saveCreds } = await useMultiFileAuthState(__dirname+'/session')
+            const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+            const { version, isLatest } = await fetchLatestBaileysVersion();
+            try {
+                const session = makeWASocket({
+                    logger: pino({ level: 'fatal' }),
+            printQRInTerminal: true,
+            browser: ['Secktor', 'safari', '1.0.0'],
+            fireInitQueries: false,
+            shouldSyncHistoryMessage: false,
+            downloadHistory: false,
+            syncFullHistory: false,
+            generateHighQualityLinkPreview: true,
+            auth: state,
+            version: version,
+            getMessage: async key => {
+                if (store) {
+                    const msg = await store.loadMessage(key.remoteJid, key.id, undefined)
+                    return msg.message || undefined
+                }
+                return {
+                    conversation: 'An Error Occurred, Repeat Command!'
+                }
+            }
+        })
+        store.bind(session.ev)
+  
+                 //------------------------------------------------------ 
+  
+                 session.ev.on("connection.update", async (s) => { 
+                     const { 
+                         connection, 
+                         lastDisconnect 
+                     } = s 
+                     if (connection == "open") { 
+  
+ async function news() { 
+  
+     let response = await fetch('https://hirunews.aquaapk-dl.repl.co/api/latest'); 
+     let data = await response.json(); 
+let mg =`*${data.title}* 
+●━━━━━━━━━━━━━━━━━━━━━●  
+${data.desc} 
+●━━━━━━━━━━━━━━━━━━━━━● 
+${data.time}
+●━━━━━━━━━━━━━━━━━━━━━●
 
-    cluster.setupMaster({
-        exec: path.join(__dirname, file),
-        args: args.slice(1),
-    })
-    let p = cluster.fork()
-    p.on('message', data => {
-        console.log('[RECEIVED]', data)
-        switch (data) {
-            case 'reset':
-                p.kill()
-                isRunning = false
-                start.apply(this, arguments)
-                break
-            case 'uptime':
-                p.send(process.uptime())
-                break
-        }
-    })
-    p.on('exit', code => {
-        isRunning = false
-        console.error('Exited with code:', code)
-        if (code === 0) return
-        fs.watchFile(args[0], () => {
-            fs.unwatchFile(args[0])
-            start(file)
-    })
-    })
+🗞️ *News From hirunews.lk*
 
-}
+🔗 *Create By Nipuna Rangana*
 
+📍 *SL News*
 
-start('main.js')
+📰 *Group Link* :- https://chat.whatsapp.com/D0ywz4bax49AVnBLOZL7sI
+
+●━━━━━━━━━━━━━━━━━━━━━●`
+     let newss = await news1.findOne({ id: '123' }) 
+  
+     if (!newss) { 
+         await new news1({ id: '123', newsid: data.id, events:'true' }).save() 
+           await session.sendMessage("94725577803@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
+     } else { 
+         if(newss.newsid == data.id )  
+          { 
+           return 
+          } 
+          else{ 
+             await news1.updateOne({ id: '123' }, { newsid : data.id, events:'true'}) 
+             await session.sendMessage("94725577803@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
+          } 
+  
+     } 
+  
+ } 
+ setInterval(news, 10000);  
+  
+                     } 
+                     if ( 
+                         connection === "close" && 
+                         lastDisconnect && 
+                         lastDisconnect.error && 
+                         lastDisconnect.error.output.statusCode != 401 
+                     ) { 
+                         XAsena() 
+                     } 
+                 }) 
+                 session.ev.on('creds.update', 
+                     saveCreds) 
+          
+session.ev.on("messages.upsert", 
+                     () => {}) 
+  
+             }catch(err) { 
+                 console.log( 
+                     err + " 😪Error Occured Please report to Owner and Stay tuned" 
+                 ); 
+             } 
+  
+  
+         } 
+         XAsena()
